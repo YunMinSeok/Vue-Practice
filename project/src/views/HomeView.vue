65cfd6e32925dee4fa2d4a617fd0e1bb60a3fff9
윤민석 <ykmvm147@naver.com>
2022-09-26 22:26:11 +0900

    feat: Change input value

--- project/src/views/HomeView.vue
+++ project/src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     {{ title }}
+    <input type="text" v-model="input1" />
   </div>
 </template>
 
@@ -14,6 +15,7 @@ export default {
   data() {
     return {
       title: "이게 vue다",
+      input1: "abc",
     };
   },
 };
