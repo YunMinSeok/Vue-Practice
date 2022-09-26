7ff0e78d2482aaa7a5f2e46b5390096a4927d6b9
윤민석 <ykmvm147@naver.com>
2022-09-26 22:22:57 +0900

    feat: Update data binding

--- project/src/views/HomeView.vue
+++ project/src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    {{ title }}
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "HomeView",
   components: {
     HelloWorld,
+  },
+  data() {
+    return {
+      title: "이게 vue다",
+    };
   },
 };
 </script>
