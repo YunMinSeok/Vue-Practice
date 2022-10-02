4395fbe6ec0881a427f4b2dd6f288306b9be532a
윤민석 <ykmvm147@naver.com>
2022-10-02 19:41:56 +0900

    feat: Update vue getbytext test

--- project/tests/HomeView.js
+++ project/tests/HomeView.js
@@ -2,4 +2,5 @@ import HomeView from "../src/views/HomeView";
 import { render } from "@testing-library/vue";
 test("It renders correctly", () => {
   const { getByText } = render(HomeView);
+  getByText("set");
 });
