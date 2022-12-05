d6f6ed0d55b80ee3b5fa7dc8c2c8c6165964c68a
윤민석 <ykmvm147@naver.com>
2022-12-05 21:12:32 +0900

    feat: Modal app.vue  렌더링 시키기

--- practice01/src/App.vue
+++ practice01/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Modal :text="dd" />
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import Modal from "./components/Modal.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    Modal,
   },
 };
 </script>
