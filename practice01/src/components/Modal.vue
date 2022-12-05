3d6082a58545d0e6e441425ea8f007d5ccf4acfe
윤민석 <ykmvm147@naver.com>
2022-12-05 21:11:46 +0900

    feat: Modal 기본 모양 만들고 렌더링 시키기

---
+++ practice01/src/components/Modal.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="wrap">
+    <span>{{ text }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  props: {
+    text: String,
+  },
+};
+</script>
+
+<style>
+.wrap {
+  position: fixed;
+  left: 0;
+  top: 35%;
+  right: 0;
+  margin: 0 auto;
+  width: 500px;
+  height: 500px;
+}
+</style>
