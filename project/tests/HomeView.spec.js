0792287bc06ed726a205be36cf13b1ca4c79ce9d
윤민석 <ykmvm147@naver.com>
2022-12-05 21:06:12 +0900

    feat: eslint 에러 잡는 중

--- project/tests/HomeView.spec.js
+++ project/tests/HomeView.spec.js
@@ -4,7 +4,6 @@ import HomeView from "../src/views/HomeView.vue";
 describe("HelloView Test", () => {
   test("It renders correctly", () => {
     const { getByText } = render(HomeView);
-    console.log(1);
     getByText("set");
   });
 });
